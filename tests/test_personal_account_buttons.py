19ccf104b5fc9ad7056cc8c589544ed415ba88f5
Stepan_Y <stepan.yakovlev.b@yandex.ru>
2024-02-21 23:03:20 +0300

    This is fine?)

--- tests/test_personal_account_buttons.py
+++ tests/test_personal_account_buttons.py
@@ -12,7 +12,6 @@ class TestAccountsButtons(Locators):
         driver.find_element(*self.PERSONAL_ACCOUNT_BUTTON).click()  # кнопка "Личный кабинет"
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((self.SAVE_BUTTON_ON_PERSONAL_PAGE)))
         assert driver.find_element(*self.SAVE_BUTTON_ON_PERSONAL_PAGE).text == 'Сохранить'
-        driver.quit()
 
     def test_constructor_buttton_on_personal_account_page(self, driver):
         driver.find_element(*self.PERSONAL_ACCOUNT_BUTTON).click()  # кнопка "Личный кабинет"
@@ -22,7 +21,6 @@ class TestAccountsButtons(Locators):
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((self.SAVE_BUTTON_ON_PERSONAL_PAGE)))
         driver.find_element(*self.CONSTRUCTOR_BUTTON).click()  # кнопка "Конструктор"
         assert driver.find_element(*self.PERSONAL_ACCOUNT_BUTTON).text == 'Личный Кабинет'
-        driver.quit()
 
     def test_logotip_on_personal_account_page(self, driver):
         driver.find_element(*self.PERSONAL_ACCOUNT_BUTTON).click()  # кнопка "Личный кабинет"
@@ -32,7 +30,6 @@ class TestAccountsButtons(Locators):
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((self.SAVE_BUTTON_ON_PERSONAL_PAGE)))
         driver.find_element(*self.LOGO_BURGERS).click()  # Логотип в хедере
         assert driver.find_element(*self.PERSONAL_ACCOUNT_BUTTON).text == 'Личный Кабинет'
-        driver.quit()
 
     def test_log_out_from_personal_account_page(self, driver):
         driver.find_element(*self.PERSONAL_ACCOUNT_BUTTON).click()  # кнопка "Личный кабинет"
@@ -43,4 +40,3 @@ class TestAccountsButtons(Locators):
         driver.find_element(*self.LOG_OUT_BUTTON).click()  # кнопка "Выход"
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((self.ENTER_BUTTON)))
         assert driver.find_element(*self.ENTER_BUTTON).text == 'Войти'
-        driver.quit()
